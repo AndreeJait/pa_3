2465333ee3a5d3511d56125b123c39c529267529
AndreeJait <panjaitanandree@gmail.com>
2022-05-19 12:30:46 +0700

    Solve Bottom Navigation Bar

--- lib/constans/router_visitor.dart
+++ lib/constans/router_visitor.dart
@@ -1,2 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:pa_3/model/bottom_navigator.dart';
+
 const routeHome = "HOME";
 const routeAbout = "ABOUT";
+
+var bottomNavigators = [
+  BottomNavigatorModel(
+      name: "Home",
+      initial: routeHome,
+      icon: const FaIcon(
+        FontAwesomeIcons.house,
+        size: 18,
+        color: Colors.black87,
+      )),
+  BottomNavigatorModel(
+      name: "About",
+      initial: routeAbout,
+      icon: const FaIcon(
+        FontAwesomeIcons.circleExclamation,
+        size: 18,
+        color: Colors.black87,
+      )),
+];
