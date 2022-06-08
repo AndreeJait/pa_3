bddec9af5d8dfa4a97fc7681f18e35ab8eec7f09
AndreeJait <panjaitanandree@gmail.com>
2022-06-08 13:40:26 +0700

    Clean

--- lib/component/bottom_navigator.dart
+++ lib/component/bottom_navigator.dart
@@ -36,7 +36,7 @@ class BottomNavigator extends StatelessWidget {
             color: Colors.grey.withOpacity(0.5),
             spreadRadius: 5,
             blurRadius: 7,
-             offset: const Offset(0, 3), /
+            offset: const Offset(0, 3),
           ),
         ],
       ),
